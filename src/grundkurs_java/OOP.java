671dabff19f3c37fc78563bbc01a09457b6f2c16
Max Demian <mikar@gmx.de>
2014-06-04 17:37:02 +0200

    herpderp still chap 8

--- src/grundkurs_java/OOP.java
+++ src/grundkurs_java/OOP.java
@@ -3,27 +3,48 @@ package grundkurs_java;
 public class OOP {
 
 	public static void main(String[] args) {
-		Student studi = new Student();
+		Student studi = Student.createStudent();
 		students(studi);
-		System.out.println(studi.getName());
 	}
 
 	private static void students(Student studi) {
 		studi.setName("Karla Karlsson");
 		studi.setNumber(12345);
+		System.out.println(studi.validateNumber());
+		System.out.println(studi); // every object has toString()
+		System.out.println(Student.getCounter()); // every object has toString()
+
 	}
 }
 
 class Student {
+	// encapsulation.
 	private String name;
 	private int number;
+	private static int counter = 0;
+
+	public static int getCounter() {
+		return counter;
+	}
+
+	public static Student createStudent() {
+		counter++;
+		return new Student();
+	}
+
+	public String toString() {
+		return name + " (" + number + ')';
+	}
 
 	public int getNumber() {
 		return this.number;
 	}
 
 	public void setNumber(int num) {
-		this.number = num;
+		int oldNumber = number;
+		number = num;
+		if (!validateNumber())
+			number = oldNumber;
 	}
 
 	public String getName() {
@@ -32,6 +53,10 @@ class Student {
 
 	public void setName(String str) {
 		this.name = str;
+	}
+
+	public boolean validateNumber() {
+		return (number >= 10000 && number <= 99999 && number % 2 != 0);
 	}
 }
 
