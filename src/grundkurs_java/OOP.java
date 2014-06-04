3f5938b1d2fdaae98e6d2743495f37eaffe76e06
Max Demian <mikar@gmx.de>
2014-06-04 15:05:52 +0200

    passing instance

--- src/grundkurs_java/OOP.java
+++ src/grundkurs_java/OOP.java
@@ -3,18 +3,40 @@ package grundkurs_java;
 public class OOP {
 
 	public static void main(String[] args) {
-		students();
-	}
-
-	private static void students() {
 		Student studi = new Student();
-		studi.name = "Karla Karlsson";
-		studi.number = 12345;
+		students(studi);
+		System.out.println(studi.getName());
 	}
 
+	private static void students(Student studi) {
+		studi.setName("Karla Karlsson");
+		studi.setNumber(12345);
+	}
 }
 
 class Student {
-	public String name;
-	public int number;
+	private String name;
+	private int number;
+
+	public int getNumber() {
+		return this.number;
+	}
+
+	public void setNumber(int num) {
+		this.number = num;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String str) {
+		this.name = str;
+	}
+}
+
+class Test extends OOP {
+	public static void main(String[] args) {
+		students(); // not visible
+	}
 }
