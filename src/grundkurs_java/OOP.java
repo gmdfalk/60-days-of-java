14619336f772a9782e2024888cdd9e2c5cf8009f
Max Demian <mikar@gmx.de>
2014-06-04 14:54:35 +0200

    starting on chapter 8 (class usage)

--- src/grundkurs_java/OOP.java
+++ src/grundkurs_java/OOP.java
@@ -3,7 +3,18 @@ package grundkurs_java;
 public class OOP {
 
 	public static void main(String[] args) {
-
+		students();
 	}
 
+	private static void students() {
+		Student studi = new Student();
+		studi.name = "Karla Karlsson";
+		studi.number = 12345;
+	}
+
+}
+
+class Student {
+	public String name;
+	public int number;
 }
