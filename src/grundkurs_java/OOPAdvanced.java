a84e3db89fd52814bc7678f0da0329c415e30783
Max Demian <mikar@gmx.de>
2014-06-09 22:35:59 +0200

    herp

--- src/grundkurs_java/OOPAdvanced.java
+++ src/grundkurs_java/OOPAdvanced.java
@@ -1,14 +1,19 @@
 package grundkurs_java;
 
+import java.util.Iterator;
+
 public class OOPAdvanced {
 
 	public static void main(String[] args) {
-
+		Euro a = new Euro(1000);
+		DM b = new DM(1);
+		System.out.println(b.euroBetrag());
 	}
 
 }
 
 // abstract = not instantiable. used often to inherit from.
+// final = not inheritable (just not mutable, in general).
 abstract class Waehrung {
 	public abstract double dollarBetrag();
 }
@@ -16,8 +21,8 @@ abstract class Waehrung {
 class USDollar extends Waehrung {
 	private double betrag;
 
-	public USDollar(double wert) {
-		this.betrag = wert;
+	public USDollar(double betrag) {
+		this.betrag = betrag;
 	}
 
 	public double dollarBetrag() {
@@ -41,5 +46,80 @@ class Yen extends Waehrung {
 
 	public static void setKurs(double Kurs) {
 		kurs = Kurs;
+	}
+}
+
+/** Die Waehrung Europas */
+class Euro extends Waehrung {
+	private static double kurs = 1;
+	private double betrag;
+
+	/** Konstruktor */
+	public Euro(double betrag) {
+		this.betrag = betrag;
+	}
+
+	public double dollarBetrag() {
+		return betrag * kurs;
+	}
+
+	public double euroBetrag() {
+		return betrag;
+	}
+
+	public static void setEuroKurs(double Kurs) {
+		kurs = Kurs;
+	}
+}
+
+class DM extends Euro {
+
+	public DM(double betrag) {
+		super(betrag / 1.95583);
+	}
+
+	public DM(Euro euro) {
+		super(euro.euroBetrag());
+	}
+
+	public double waehrungsBetrag() {
+		return euroBetrag() * 1.95583;
+	}
+}
+
+class Lire extends Euro {
+
+	public Lire(double betrag) {
+		super(betrag / 1936.27);
+	}
+
+	public Lire(Euro euro) {
+		super(euro.euroBetrag());
+	}
+
+	public double waehrungsBetrag() {
+		return euroBetrag() * 1936.27;
+	}
+}
+
+class Franc extends Euro {
+
+	public Franc(double betrag) {
+		super(betrag / 6.55957);
+	}
+
+	public Franc(Euro euro) {
+		super(euro.euroBetrag());
+	}
+
+	public double waehrungsBetrag() {
+		return euroBetrag() * 6.55957;
+	}
+}
+
+class CurrencyCalc {
+	public static void main(String[] args) {
+		DM dm = new DM(13.20);
+		System.out.println(dm.euroBetrag());
 	}
 }
