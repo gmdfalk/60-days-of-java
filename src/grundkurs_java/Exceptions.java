0f526b799b310d353ce7a06e35a0c1682628e5e7
Max Demian <mikar@gmx.de>
2014-07-08 15:51:49 +0200

    assertions

--- src/grundkurs_java/Exceptions.java
+++ src/grundkurs_java/Exceptions.java
@@ -14,9 +14,54 @@ public class Exceptions {
 			System.out.println("Tried to divide by 0: " + a);
 			a = 11;
 		}
-		ReadMe.main(args);
+		// ReadMe.main(args);
+		AssertionTest.main(args);
 	}
 
+}
+
+class AssertionTest {
+	public static double kehrwert(double x) {
+		assert x != 0 : "division by 0";
+		return 1 / x;
+
+	}
+
+	public static void main(String[] args) {
+		double x = 123.45;
+		try {
+			System.out.println(kehrwert(x));
+
+		} catch (AssertionError e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
+
+class Exueb8 {
+	/** Bestimme eine Zufallszahl zwischen 0 und 0.5 */
+	public static double gibZufallszahlBisEinhalb() throws Exception {
+		double res = Math.random();
+		if (res > 0.5)
+			throw new Exception("Zahl zu gross");
+		return res;
+	}
+
+	/** Hauptprogramm */
+	public static void main(String[] args) {
+		// Bestimme eine Zufallszahl zwischen 0 und 0.5
+		double zahl;
+		try {
+			zahl = gibZufallszahlBisEinhalb();
+		}
+		// Falls etwas schief geht (Exception)
+		// verwende die Zahl 0.5
+		catch (Exception e) {
+			zahl = 0.5;
+		}
+		// gib die Zahl auf dem Bildschirm aus
+		System.out.println(zahl);
+	}
 }
 
 class DigitException extends RuntimeException {
