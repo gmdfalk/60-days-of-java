3e4ab9dc2157545e9c8e609fe7f29bff60302bcf
Max Demian <mikar@gmx.de>
2014-07-08 16:03:50 +0200

    chapter 11 (advanced oop)

---
+++ src/grundkurs_java/Chap11.java
@@ -0,0 +1,13 @@
+package grundkurs_java;
+
+public class Chap11 {
+
+	public static void main(String[] args) {
+		System.out.println(Jahreszeit.WINTER.ordinal());
+	}
+
+	public enum Jahreszeit {
+		FRUEHLING, SOMMER, HERBST, WINTER;
+	}
+
+}
