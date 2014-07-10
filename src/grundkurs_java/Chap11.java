5df57d95756704f9a352e4aa117ecb3bdff7d736
Max Demian <mikar@gmx.de>
2014-07-10 20:49:51 +0200

    suaheli

--- src/grundkurs_java/Chap11.java
+++ src/grundkurs_java/Chap11.java
@@ -64,6 +64,20 @@ public class Chap11 {
 		Socke gS = genSockenPaar.getL();
 		System.out.println(gS);
 
+		TollesPaarTestWild2.main(args);
+	}
+}
+
+class TollesPaarTestWild2 {
+	public static void paarAusgeben2(TollesPaar<?> tp) {
+		System.out.println(tp);
+	}
+
+	public static void main(String[] args) {
+		Hose ho1 = new Hose();
+		Hose ho2 = new Hose();
+		TollesPaar<Hose> p1 = new TollesPaar<Hose>(ho1, ho2);
+		paarAusgeben2(p1);
 	}
 }
 
