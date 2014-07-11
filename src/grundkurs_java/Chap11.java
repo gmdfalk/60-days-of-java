8f8c2cafcdcc5ad3e0de48c1e3eaa99dd91e8515
Max Demian <mikar@gmx.de>
2014-07-11 17:06:46 +0200

    genpaartest5

--- src/grundkurs_java/Chap11.java
+++ src/grundkurs_java/Chap11.java
@@ -68,11 +68,35 @@ public class Chap11 {
 		GenPaarTestWild1.main(args);
 		GenPaarTestWild2.main(args);
 		GenPaarTest3.main(args);
+		GenPaarTest5.main(args);
+	}
+}
+
+class GenPaarTest5 {
+	public static <T, S extends T> GenPaar<T> linksPaar(GenPaar<T> x,
+			GenPaar<S> y) {
+		return new GenPaar<T>(x.getL(), y.getL());
+	}
+
+	public static void main(String[] args) {
+		Hose h1 = new Hose();
+		Hose h2 = new Hose();
+		Jeans j1 = new Jeans();
+		Jeans j2 = new Jeans();
+		GenPaar<Hose> p1 = new GenPaar<Hose>(h1, h2);
+		GenPaar<Jeans> p2 = new GenPaar<Jeans>(j1, j2);
+		System.out.println(linksPaar(p1, p2));
 	}
 }
 
 class GenPaarTest3 {
+	// without wildcard
 	public static <T> boolean linksGleichRechts(GenPaar<T> x) {
+		return x.getL().equals(x.getR());
+	}
+
+	// with wildcard
+	public static boolean linksGleichRechts2(GenPaar<?> x) {
 		return x.getL().equals(x.getR());
 	}
 
