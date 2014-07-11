a60f3e523db0db17a54e69ba116112c421316237
Max Demian <mikar@gmx.de>
2014-07-11 14:11:20 +0200

    bound wildcards

--- src/grundkurs_java/Chap11.java
+++ src/grundkurs_java/Chap11.java
@@ -65,6 +65,26 @@ public class Chap11 {
 		System.out.println(gS);
 
 		TollesPaarTestWild2.main(args);
+		GenPaarTestWild1.main(args);
+	}
+}
+
+class Jeans extends Hose {
+	public String toString() {
+		return "Jeans";
+	}
+}
+
+class GenPaarTestWild1 {
+	public static void genPaarAusgeben1(GenPaar<? extends Hose> gp) {
+		System.out.println(gp);
+	}
+
+	public static void main(String[] args) {
+		Jeans j1 = new Jeans();
+		Jeans j2 = new Jeans();
+		GenPaar<Jeans> p1 = new GenPaar<Jeans>(j1, j2);
+		genPaarAusgeben1(p1);
 	}
 }
 
