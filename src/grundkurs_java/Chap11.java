963091dae293701a062c18c9741264c19b0dda1e
Max Demian <mikar@gmx.de>
2014-07-12 13:25:16 +0200

    11.6

--- src/grundkurs_java/Chap11.java
+++ src/grundkurs_java/Chap11.java
@@ -75,9 +75,46 @@ public class Chap11 {
 		// TierKaefig<?> kaefig = new TierKaefig<Katze>();
 		// kaefig.setInsasse(new Katze());
 		// not type safe
-		TiersKaefig kaefig = new TiersKaefig();
-		kaefig.setInsasse(new Hunds());
-		Tierleben.main(args);
+		// TiersKaefig kaefig = new TiersKaefig();
+		// kaefig.setInsasse(new Hunds());
+		// Tierleben.main(args);
+		RateMal.main(args);
+		// System.out.println(Boolean.TRUE.compareTo(Boolean.FALSE) > 0);
+		// System.out.println("welt".compareTo("schoene")); // 4
+		// System.out.println("welt".compareTo("du")); // 19
+		// System.out.println("welt".compareTo("hallo")); // 15
+		// System.out.println("schoene".compareTo("du")); // 15
+		// System.out.println("schoene".compareTo("hallo")); // 11
+		// System.out.println("du".compareTo("hallo")); // -4
+	}
+}
+
+// 11.6
+class RateMal {
+	public static void ausgabe(Object... eingabe) {
+		System.out.print("Ausgabe: ");
+		for (Object o : eingabe)
+			System.out.print(o + " ");
+		System.out.println();
+	}
+
+	public static <T extends Comparable> T[] tueWas(T... eingabe) {
+		eingabe = eingabe.clone();
+		for (int i = eingabe.length - 1; i > 0; i--)
+			for (int j = 0; j < i; j++)
+				if (eingabe[j].compareTo(eingabe[j + 1]) > 0) {
+					T tmp = eingabe[j];
+					eingabe[j] = eingabe[j + 1];
+					eingabe[j + 1] = tmp;
+				}
+		return eingabe;
+	}
+
+	public static void main(String[] args) {
+		ausgabe(tueWas(Boolean.TRUE, Boolean.FALSE)); // Ausgabe: false true
+		ausgabe(tueWas("welt", "schoene", "du", "hallo")); // Ausgabe: du hallo
+															// schoene
+															// welt
 	}
 }
 
