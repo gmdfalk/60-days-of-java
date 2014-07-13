cd203c583964707eafc412ed5e869ca51839850e
Max Demian <mikar@gmx.de>
2014-07-13 13:25:25 +0200

    12.2

--- src/grundkurs_java/Chap12.java
+++ src/grundkurs_java/Chap12.java
@@ -3,25 +3,38 @@ package grundkurs_java;
 public class Chap12 {
 
 	public static void main(String[] args) {
-		StringOps.main(args);
+		StringTiming.main(args);
+		StringManipulation
+				.main("In diesem Grundkurs haben wir so viel gelernt");
 	}
 
 }
 
-class StringOps {
-
-	static {
-		Runtime.getRuntime().addShutdownHook(new Thread() {
-			public void run() {
-				try {
-
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+class StringManipulation {
+	public static void main(String sentence) {
+		StringBuffer result = new StringBuffer();
+		for (int i = 0; i < sentence.length(); i++) {
+			if (sentence.charAt(i) == 'a' || sentence.charAt(i) == 'e'
+					|| sentence.charAt(i) == 'E' || sentence.charAt(i) == 'A') {
+				continue;
+			} else if (sentence.charAt(i) == 'i' || sentence.charAt(i) == 'o'
+					|| sentence.charAt(i) == 'I' || sentence.charAt(i) == 'O') {
+				result.append(sentence.charAt(i));
+				result.append(sentence.charAt(i));
+			} else if (sentence.charAt(i) == 'u') {
+				result.append("x");
+			} else if (sentence.charAt(i) == 'U') {
+				result.append("X");
+			} else {
+				result.append(sentence.charAt(i));
 			}
-		});
+		}
+		System.out.println(result);
 	}
+}
 
+class StringTiming {
+	// 12.1
 	public static void nobuffer(int iterations) {
 		final long startTime = System.currentTimeMillis();
 		String str = "";
