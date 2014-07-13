69838519ccc27e50f23a2011f7bfcf38baf54eb2
Max Demian <mikar@gmx.de>
2014-07-13 12:42:39 +0200

    finished chap 11

--- src/grundkurs_java/Chap11.java
+++ src/grundkurs_java/Chap11.java
@@ -86,7 +86,73 @@ public class Chap11 {
 		// System.out.println("schoene".compareTo("du")); // 15
 		// System.out.println("schoene".compareTo("hallo")); // 11
 		// System.out.println("du".compareTo("hallo")); // -4
-		Konzert.main(args);
+		// Konzert.main(args);
+		RingDemo.main(args);
+	}
+}
+
+/** Erzeugt zehn zufaellige Ringe und sortiert sie */
+class RingDemo {
+	public static void main(String[] args) {
+		Ring[] ringe = new Ring[10];
+		// Erzeuge 10 Ringe
+		for (int i = 0; i < ringe.length; i++)
+			ringe[i] = new Ring(Math.random());
+		java.util.Arrays.sort(ringe);
+		// Sortiere die Ringe
+		for (Ring r : ringe)
+			System.out.println(r);
+		// Gib die Ringe aus
+	}
+}
+
+/**
+ * Diese Klasse repraesentiert Ringobjekte, die nach ihrer Groesse sortiert
+ * werden koennen.
+ */
+class Ring implements Comparable<Ring> {
+	/** Durchmesser */
+	private double durchmesser;
+
+	/** Konstruktor */
+	public Ring(double durchmesser) {
+		this.durchmesser = durchmesser;
+	}
+
+	/** Gibt den Durchmesser in der toString-Methode aus */
+	public String toString() {
+		return "Ring der Groesse " + durchmesser;
+	}
+
+	public int compareTo(Ring o) {
+		double durchmesser2 = o.durchmesser;
+		if (durchmesser < durchmesser2)
+			return -1;
+		// Fall 1
+		if (durchmesser > durchmesser2)
+			return 1;
+		// Fall 2344
+		return 0;
+		// Fall 3
+	}
+
+	/** Die equals-Methode muss auf den Vergleich abgestimmt werden */
+	public boolean equals(Object o) {
+		if (o == null)
+			return false;
+		if (this == o)
+			return true;
+		if (getClass() != o.getClass())
+			return false;
+		return compareTo((Ring) o) == 0;
+	}
+
+	/**
+	 * Wer die equals-Methode veraendert, muss auch die hashCode-Methode
+	 * veraendern.
+	 */
+	public int hashCode() {
+		return (new Double(durchmesser)).hashCode();
 	}
 }
 
