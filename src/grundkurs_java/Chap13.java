f4e41b39ed2d0929607e4cb8317131236c5cce78
Max Demian <mikar@gmx.de>
2014-07-19 08:10:52 +0200

    starting on Chapter 13 (GUI)

---
+++ src/grundkurs_java/Chap13.java
@@ -0,0 +1,14 @@
+package grundkurs_java;
+
+import java.awt.*;
+
+public class Chap13 {
+
+	public static void main(String[] args) {
+		Frame fenster = new Frame();
+		fenster.setTitle("Bla");
+		fenster.setSize(300, 150);
+		fenster.setVisible(true);
+	}
+
+}
