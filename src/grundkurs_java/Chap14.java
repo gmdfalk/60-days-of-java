25b7ae17a99ca43e56ab0c0e76697770e57530b8
Max Demian <mikar@gmx.de>
2014-07-20 20:58:11 +0200

    kitten! also some code

--- src/grundkurs_java/Chap14.java
+++ src/grundkurs_java/Chap14.java
@@ -11,7 +11,71 @@ public class Chap14 {
 		// FrameMitMonospacedText.main(args);
 		// FrameMitFlowLayout.main(args);
 		// FrameMitBorderLayout.main(args);
-		FrameMitGridLayout.main(args);
+		// FrameMitGridLayout.main(args);
+		FrameMitBild.main(args);
+	}
+}
+
+class FrameMitButtons extends JFrame {
+	Container c;
+	// Container dieses Frames
+	// Feld fuer Buttons, die im Frame erscheinen sollen
+	JButton b[] = new JButton[4];
+
+	public FrameMitButtons() {
+		// Konstruktor
+		c = getContentPane();
+		// Container bestimmen
+		c.setLayout(new FlowLayout());
+		// Layout setzen
+		// Erzeuge die Button-Objekte
+		for (int i = 0; i < 4; i++) {
+			b[i] = new JButton("Taste " + (i + 1));
+			b[i].setFont(new Font("SansSerif", Font.ITALIC, 24));
+		}
+		// Fuege die Buttons dem Frame hinzu
+		for (int i = 0; i < 4; i++) {
+			c.add(b[i]);
+		}
+	}
+
+	public static void main(String[] args) {
+		FrameMitButtons fenster = new FrameMitButtons();
+		fenster.setTitle("Frame mit Buttons");
+		fenster.setSize(250, 130);
+		fenster.setVisible(true);
+		fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
+
+class FrameMitBild extends JFrame {
+	Container c;
+	// Container dieses Frames
+	JLabel lab;
+
+	// Label das im Frame erscheinen soll
+	public FrameMitBild() { // Konstruktor
+		c = getContentPane();
+		// Container bestimmen
+		c.setLayout(new FlowLayout());
+		// Layout setzen
+		// Bildobjekt erzeugen
+		Icon bild = new ImageIcon("kitten.jpg");
+		// Label mit Text und Bild beschriften
+		lab = new JLabel("Spotty", bild, JLabel.CENTER);
+		// Text unter das Bild setzen
+		lab.setHorizontalTextPosition(JLabel.CENTER);
+		lab.setVerticalTextPosition(JLabel.BOTTOM);
+		// Fuege das Label dem Frame hinzu
+		c.add(lab);
+	}
+
+	public static void main(String[] args) {
+		FrameMitBild fenster = new FrameMitBild();
+		fenster.setTitle("Label mit Bild und Text");
+		fenster.setSize(250, 185);
+		fenster.setVisible(true);
+		fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
 
