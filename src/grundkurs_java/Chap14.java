4cd9f09bd3cf13a9cac324a9578f839b1489950a
Max Demian <mikar@gmx.de>
2014-07-20 21:11:12 +0200

    togglebuttons

--- src/grundkurs_java/Chap14.java
+++ src/grundkurs_java/Chap14.java
@@ -12,7 +12,43 @@ public class Chap14 {
 		// FrameMitFlowLayout.main(args);
 		// FrameMitBorderLayout.main(args);
 		// FrameMitGridLayout.main(args);
-		FrameMitBild.main(args);
+		// FrameMitBild.main(args);
+		// FrameMitButtons.main(args);
+		FrameMitToggleButtons.main(args);
+	}
+}
+
+class FrameMitToggleButtons extends JFrame {
+	Container c;
+	// Container dieses Frames
+	// Feld fuer Toggle-Buttons, die im Frame erscheinen sollen
+	JToggleButton b[] = new JToggleButton[4];
+
+	public FrameMitToggleButtons() {
+		c = getContentPane();
+		c.setLayout(new FlowLayout());
+		// Konstruktor
+		// Container bestimmen
+		// Layout setzen
+		// Erzeuge die Button-Objekte
+		for (int i = 0; i < 4; i++) {
+			b[i] = new JToggleButton("Schalter " + (i + 1));
+			b[i].setFont(new Font("SansSerif", Font.ITALIC, 24));
+		}
+		b[0].setSelected(true);
+		b[2].setSelected(true);
+		// Fuege die Buttons dem Frame hinzu
+		for (int i = 0; i < 4; i++) {
+			c.add(b[i]);
+		}
+	}
+
+	public static void main(String[] args) {
+		FrameMitToggleButtons fenster = new FrameMitToggleButtons();
+		fenster.setTitle("Frame mit Buttons");
+		fenster.setSize(330, 130);
+		fenster.setVisible(true);
+		fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
 
