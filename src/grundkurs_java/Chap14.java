40d9af91b46e99568bbe4536e1c06fc782c7c8ed
Max Demian <mikar@gmx.de>
2014-07-20 20:50:55 +0200

    GUI nordwestsüdost

--- src/grundkurs_java/Chap14.java
+++ src/grundkurs_java/Chap14.java
@@ -6,10 +6,52 @@ import javax.swing.*;
 public class Chap14 {
 
 	public static void main(String[] args) {
-		FrameMitTextUndToolTip.main(args);
+		// FrameMitTextUndToolTip.main(args);
 		// FrameMitSchwarzemLabel.main(args);
 		// FrameMitMonospacedText.main(args);
 		// FrameMitFlowLayout.main(args);
+		FrameMitBorderLayout.main(args);
+	}
+}
+
+class FrameMitBorderLayout extends JFrame {
+	Container c;
+	// Container dieses Frames
+	// Labelfeld fuer Label, die im Frame erscheinen sollen430
+	FarbigesLabel fl[] = new FarbigesLabel[5];
+
+	public FrameMitBorderLayout() {
+		// Konstruktor
+		c = getContentPane();
+		// Container bestimmen
+		c.setLayout(new BorderLayout());
+		// Layout setzen
+		/* Erzeuge die Labelobjekte mit Text und Farbe */
+		fl[0] = new FarbigesLabel("Norden", Color.BLACK, Color.WHITE);
+		fl[1] = new FarbigesLabel("Sueden", Color.WHITE, Color.LIGHT_GRAY);
+		fl[2] = new FarbigesLabel("Osten", Color.WHITE, Color.GRAY);
+		fl[3] = new FarbigesLabel("Westen", Color.WHITE, Color.DARK_GRAY);
+		fl[4] = new FarbigesLabel("Zentrum", Color.WHITE, Color.BLACK);
+		for (int i = 0; i < 5; i++) {
+			// Setze die Schriftart der Labelbeschriftung
+			fl[i].setFont(new Font("SansSerif", Font.BOLD, 14));
+			// Setze die horizontale Position des Labeltextes auf dem Label
+			fl[i].setHorizontalAlignment(JLabel.CENTER);
+		}
+		// Fuege die Labels dem Frame hinzu
+		c.add(fl[0], BorderLayout.NORTH);
+		c.add(fl[1], BorderLayout.SOUTH);
+		c.add(fl[2], BorderLayout.EAST);
+		c.add(fl[3], BorderLayout.WEST);
+		c.add(fl[4], BorderLayout.CENTER);
+	}
+
+	public static void main(String[] args) {
+		FrameMitBorderLayout fenster = new FrameMitBorderLayout();
+		fenster.setTitle("Frame mit Border-Layout");
+		fenster.setSize(300, 150);
+		fenster.setVisible(true);
+		fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
 
