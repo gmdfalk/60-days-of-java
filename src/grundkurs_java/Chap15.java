d1d7b383cc0cacdf783b0d4e8e5b0cbfef9d728a
Max Demian <mikar@gmx.de>
2014-07-22 18:27:40 +0200

    Adapter

--- src/grundkurs_java/Chap15.java
+++ src/grundkurs_java/Chap15.java
@@ -10,9 +10,59 @@ public class Chap15 {
 		// Farbwechsel.main(args);
 		// Bilderrahmen.main(args);
 		// Farbwechsel2.main(args);
-		Farbwechsel4.main(args);
+		// Farbwechsel4.main(args);
+		CloseToggleButtons.main(args);
 	}
 
+}
+
+class CloseToggleButtons extends JFrame {
+	// Extending its adapter instead of implementing a Listener
+	Container c;
+	JLabel l;
+	JToggleButton b1, b2;
+
+	// Container dieses Frames
+	// Label
+	// Toggle-Buttons
+	public CloseToggleButtons() { // Konstruktor
+		c = getContentPane();
+		// Container bestimmen
+		c.setLayout(new FlowLayout());
+		// Layout setzen
+		// Erzeuge die Label- und Button-Objekte
+		l = new JLabel("Zum Schliessen des Fensters "
+				+ "beide Schalter aktivieren!");
+		b1 = new JToggleButton("Schalter 1");
+		b2 = new JToggleButton("Schalter 2");
+		// Fuege die Komponenten dem Frame hinzu
+		c.add(l);
+		c.add(b1);
+		c.add(b2);
+		// Registriere WindowListener beim Frame
+		addWindowListener(new ClosingListener());
+	}
+
+	// Innere Listener-Klasse
+	public class ClosingListener extends WindowAdapter {
+		public void windowClosing(WindowEvent e) {
+			if (b1.isSelected() && b2.isSelected()) {
+				e.getWindow().dispose();
+				System.exit(0);
+			} else
+				JOptionPane.showMessageDialog(c,
+						"Vor dem Schliessen erst beide Schalter aktivieren!");
+		}
+	}
+
+	public static void main(String[] args) {
+		CloseToggleButtons fenster = new CloseToggleButtons();
+		fenster.setTitle("CloseToggleButtons");
+		fenster.setSize(400, 100);
+		fenster.setVisible(true);
+		// Setze das Verhalten des Frames beim Schliessen auf "Nichtstun"
+		fenster.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+	}
 }
 
 class Bilderrahmen extends JFrame {
