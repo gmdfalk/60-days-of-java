998268b79c426be8f6a9f70de3249f23d3b37af7
Max Demian <mikar@gmx.de>
2014-07-27 08:50:14 +0200

    nils holgersson

--- src/grundkurs_java/Chap18.java
+++ src/grundkurs_java/Chap18.java
@@ -4,9 +4,40 @@ public class Chap18 {
 
 	public static void main(String[] args) {
 		// MehrmalsP.main(args);
-		MehrmalsT.main(args);
+		// MehrmalsT.main(args);
+		TVProgAuslosung.main(args);
 	}
 
+}
+
+class TVProgAuslosung {
+	public static void main(String[] args) {
+		TVProgThread t1 = new TVProgThread("Wer wird Millionaer?");
+		TVProgThread t2 = new TVProgThread("Enterprise");
+		TVProgThread t3 = new TVProgThread("Nils Holgersson");
+		t1.start();
+		t2.start();
+		t3.start();
+	}
+}
+
+class TVProgThread extends Thread {
+	// Konstruktor
+	public TVProgThread(String name) {
+		super(name);
+	}
+
+	// run-Methode (Schleife mit Zufalls-Wartezeiten)
+	public void run() {
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(getName() + " zum " + i + ". Mal");
+			try {
+				sleep((int) (Math.random() * 1000));
+			} catch (InterruptedException e) {
+			}
+		}
+		System.out.println(getName() + " FERTIG!");
+	}
 }
 
 class MehrmalsT {
@@ -23,11 +54,7 @@ class ABCThread extends Thread {
 			// Gib den Buchstaben aus
 			System.out.print(b);
 			// Verbringe eine Sekunde mit "Nichtstun"
-			try {
-				sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+			MachMal.eineSekundeLangGarNichts();
 		}
 	}
 }
@@ -46,15 +73,21 @@ class ABCPrinter {
 			// Gib den Buchstaben aus
 			System.out.print(b);
 			// Verbringe eine Sekunde mit "Nichtstun"
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException ex) {
-				Thread.currentThread().interrupt();
-			}
+			MachMal.eineSekundeLangGarNichts();
 		}
 	}
 
 	public void start() {
 		run();
+	}
+}
+
+class MachMal {
+	public static void eineSekundeLangGarNichts() {
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
 	}
 }
