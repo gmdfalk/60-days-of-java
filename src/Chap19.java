b0947a7ae315de42b849a0313925cb41fcf096b3
Max Demian <mikar@gmx.de>
2014-07-28 18:59:52 +0200

    datainputstream

--- src/Chap19.java
+++ src/Chap19.java
@@ -17,6 +17,30 @@ public class Chap19 {
 
 }
 
+class DataWriteAndRead {
+	// Speichert elementare Werte in einer Datei und liest sie wieder ein
+	public static void main(String[] args) {
+		try {
+			File datei = new File("binaer.dat");
+			FileOutputStream out = new FileOutputStream(datei);
+			DataOutputStream dout = new DataOutputStream(out);
+			dout.writeInt(1);
+			dout.writeDouble(2.3);
+			dout.writeChar('a');
+			dout.writeBoolean(true);
+			dout.close();
+			FileInputStream in = new FileInputStream(datei);
+			DataInputStream din = new DataInputStream(in);
+			System.out.println("int: " + din.readInt());
+			System.out.println("double: " + din.readDouble());
+			System.out.println("char: " + din.readChar());
+			System.out.println("boolean: " + din.readBoolean());
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+	}
+}
+
 class Eingaben {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
